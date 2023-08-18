d12cc7a72a21a11f84f61bfe197ecd3a3c4699ac
Eric Chan <ericchan@yoov.com>
2023-08-18 09:48:21 +0800

    remove unnessary

--- inference-web.sh
+++ inference-web.sh
@@ -1,4 +1,1 @@
-export WEB_USERNAME=user
-export WEB_PASSWORD=user
-
 python inference.py --model_type llama --instruction "play in tales fairy" --model_path "LLMs/open-llama/open-llama-3b" --adapter_weights "output/llama" --max_new_tokens 128
